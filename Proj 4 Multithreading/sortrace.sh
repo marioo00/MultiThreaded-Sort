8fc569338e479677de30b4d48d625f2e72ff9b95
marioo00 <154845002+marioo00@users.noreply.github.com>
2025-02-22 21:43:00 -0800

    Update sortrace.sh

--- Proj 4 Multithreading/sortrace.sh
+++ Proj 4 Multithreading/sortrace.sh
@@ -1,7 +1,7 @@
 # Name: Mario Robles
 # Class (CECS 325-02)
 # Project Name (Prog 4 – Threads)
-# Due Date (10/31/2024)
+# 
 #
 #I certify that this program is my own original work. I did not copy any part of this program from
 # any other source. I further certify that I typed each and every line of code in this program.
